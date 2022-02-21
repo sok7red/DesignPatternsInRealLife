d6fdedccb704a9a7145d8a5433210312b28c1c78
Theo Sokos <theo.sokos@rabobank.com>
2022-02-21 13:52:35 +0000

    Updated the Runner to be able to generate the economic entities before the calculation step.

--- Adaptation_Builder_Immutable/Adaptation_Builder_Immutable/Domain/RiskCalculationRunner.cs
+++ Adaptation_Builder_Immutable/Adaptation_Builder_Immutable/Domain/RiskCalculationRunner.cs
@@ -1,16 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using Adaptation_Builder_Immutable.Enums;
+using Adaptation_Builder_Immutable.Interfaces;
 
 namespace Adaptation_Builder_Immutable.Domain
 {
     public class RiskCalculationRunner
     {
-
-        public void Run()
+        private readonly IList<IEconomicEntity> _economicEntities = new List<IEconomicEntity>();
+        private IRiskDataDao RiskDataDao { get; set; }
+        private IMarketDao MarketDataDao { get; set; }
+        private IEnumerable<IEligibilityInformation> EligibilityInformation { get; set; }
+        private Dictionary<CreditRatings, CreditQualitySteps> CreditQualities { get; set; }
+        private Dictionary<CreditQualitySteps, double> RiskWeigts { get; set; }
+        private void CreateEconomicEntities(IEnumerable<IBasicEntityObject> objectsToAdaptFrom)
         {
-
+            foreach (var basicObect in objectsToAdaptFrom)
+            {
+                _economicEntities.Add(
+                    new SimpleAdaptor(RiskWeigts, CreditQualities, EligibilityInformation).AdaptFrom(basicObect));
+            }
         }
 
+        public RiskCalculationRunner(IRiskDataDao riskDataDao, IMarketDao marketDataDao)
+        {
+            RiskDataDao = riskDataDao;
+            MarketDataDao = marketDataDao;
+        }
+
+        public void Run(IEnumerable<IBasicEntityObject> objectsToAdaptFrom)
+        {
+            RiskWeigts = RiskDataDao.GetRiskweights();
+            CreditQualities = RiskDataDao.GetCreditQualities();
+            EligibilityInformation = RiskDataDao.GetEligibilityInformation();
+
+            CreateEconomicEntities(objectsToAdaptFrom);
+
+            /* Calculation will be added here*/
+        }
+    }
+
+    public interface IMarketDao
+    {
+    }
+
+    public interface IRiskDataDao
+    {
+        Dictionary<CreditQualitySteps, double> GetRiskweights();
+
+        Dictionary<CreditRatings, CreditQualitySteps> GetCreditQualities();
+
+        IEnumerable<IEligibilityInformation> GetEligibilityInformation();
     }
 }
